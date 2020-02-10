de2af4e1c7c846e43174f38e662d6a76240fe8cf
Lúcás Meier <cronokirby@gmail.com>
2020-02-10 10:29:55 +0100

    Simplify deploys

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
-hugo
+~/go/bin/hugo
 cd public
+git init 
 git add .
 git commit -m "Rebuilding site"
-git push origin master
+git remote add origin git@github.com:cronokirby/cronokirby.github.io
+git push --force origin master
 cd ..
